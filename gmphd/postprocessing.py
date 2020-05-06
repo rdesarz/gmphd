38062b76076b513a765f14668dfe47bef0cf206b
rdesarz <hib.pub@protonmail.com>
2020-05-06 17:31:46 +0200

    Add merge step

--- gmphd/postprocessing.py
+++ gmphd/postprocessing.py
@@ -1,3 +1,49 @@
+from numpy.linalg import inv
+from math import fsum
+from gmphd.gaussian_component import GaussianComponent
+from collections import namedtuple
+
+
 def prune(posterior_intensity, truncation_threshold):
     posterior_intensity = [gaussian for gaussian in posterior_intensity if gaussian.weight >= truncation_threshold]
     return posterior_intensity
+
+
+def merging(posterior_intensity, merge_threshold):
+    # The merging algorithm process the components in sorted order based on their weight
+    posterior_intensity.sort(key=lambda gaussian: gaussian.weight, reverse=True)
+
+    # Lookup table to check if a component was merged
+    merged = [False for i in range(len(posterior_intensity))]
+
+    # Process the gaussian components in a pyramidal fashion
+    for i in range(0, len(posterior_intensity) - 1):
+        if not merged[i]:
+            to_be_merged = [posterior_intensity[i]]
+            for j in range(i + 1, len(posterior_intensity)):
+                if not merged[j] and compute_merge_score(posterior_intensity[i],
+                                                         posterior_intensity[j]) < merge_threshold:
+                    to_be_merged.append(posterior_intensity[j])
+                    merged[j] = True
+
+                posterior_intensity[i] = merge_gaussians(to_be_merged)
+
+    posterior_intensity = [component for merged, component in zip(merged, posterior_intensity) if not merged]
+
+    return posterior_intensity
+
+
+def merge_gaussians(gaussians_list):
+    weight = fsum(gaussian.weight for gaussian in gaussians_list)
+    mean = sum(gaussian.mean.dot(gaussian.weight) for gaussian in gaussians_list).dot(1. / weight)
+    covariance = sum(
+        (gaussian.covariance + (mean - gaussian.mean).transpose().dot((mean - gaussian.mean))).dot(gaussian.weight) for
+        gaussian in gaussians_list).dot(
+        1. / weight)
+
+    return GaussianComponent(weight=weight, mean=mean, covariance=covariance)
+
+
+def compute_merge_score(gaussian_i, gaussian_j):
+    mean_diff = (gaussian_i.mean - gaussian_j.mean)
+    return mean_diff.transpose().dot(inv(gaussian_i.covariance).dot(mean_diff))
--- tests/test_postprocessing.py
+++ tests/test_postprocessing.py
@@ -25,3 +25,91 @@ class TestPostprocessing(TestCase):
         self.assertAlmostEqual(posterior_intensity[0].weight, 1.)
         self.assertAlmostEqual(posterior_intensity[1].weight, truncation_threshold)
 
+    def test_merge_score_same_gaussian(self):
+        gaussian = GaussianComponent(gaussian_mean, gaussian_cov, gaussian_weight)
+
+        score = compute_merge_score(gaussian, gaussian)
+
+        self.assertAlmostEqual(score, 0.)
+
+    def test_merge_score_different_gaussian(self):
+        gaussian_1 = GaussianComponent(gaussian_mean, gaussian_cov, gaussian_weight)
+        gaussian_mean_2 = np.array([0.5, 0.5, 0.2, 0.2]).transpose()
+        gaussian_cov_2 = np.array([[1., 0., 0., 0.],
+                                   [0., 1., 0., 0.],
+                                   [0., 0., 1., 0.],
+                                   [0., 0., 0., 1.]])
+        gaussian_weight_2 = 1.
+        gaussian_2 = GaussianComponent(gaussian_mean_2, gaussian_cov_2, gaussian_weight_2)
+
+        score = compute_merge_score(gaussian_1, gaussian_2)
+
+        self.assertAlmostEqual(score, 0.68)
+
+    def test_merge_three_equal_gaussians(self):
+        gaussian_1 = GaussianComponent(gaussian_mean, gaussian_cov, 0.1)
+        gaussian_2 = GaussianComponent(gaussian_mean, gaussian_cov, 0.1)
+        gaussian_3 = GaussianComponent(gaussian_mean, gaussian_cov, 0.1)
+
+        merged_gaussian = merge_gaussians([gaussian_1, gaussian_2, gaussian_3])
+
+        self.assertAlmostEqual(merged_gaussian.weight, 0.3)
+        self.assertTrue(np.allclose(merged_gaussian.mean, gaussian_mean))
+        self.assertTrue(np.allclose(merged_gaussian.covariance, gaussian_cov))
+
+    def test_merge_intensity_with_three_gaussian_two_close(self):
+        gaussian_1 = GaussianComponent(gaussian_mean, gaussian_cov, 0.6)
+        gaussian_2 = GaussianComponent(gaussian_mean, gaussian_cov, 0.3)
+        gaussian_mean_2 = np.array([0.5, 0.5, 0.2, 0.2]).transpose()
+        gaussian_cov_2 = np.array([[1., 0., 0., 0.],
+                                   [0., 1., 0., 0.],
+                                   [0., 0., 1., 0.],
+                                   [0., 0., 0., 1.]])
+        gaussian_3 = GaussianComponent(gaussian_mean_2, gaussian_cov_2, 0.1)
+
+        posterior_intensity = [gaussian_1, gaussian_2, gaussian_3]
+
+        posterior_intensity = merging(posterior_intensity, 0.5)
+
+        self.assertEqual(len(posterior_intensity), 2)
+
+    def test_merge_intensity_with_three_gaussian_two_close_not_weight_sorted(self):
+        gaussian_1 = GaussianComponent(gaussian_mean, gaussian_cov, 0.6)
+        gaussian_2 = GaussianComponent(gaussian_mean, gaussian_cov, 0.3)
+        gaussian_mean_2 = np.array([0.5, 0.5, 0.2, 0.2]).transpose()
+        gaussian_cov_2 = np.array([[1., 0., 0., 0.],
+                                   [0., 1., 0., 0.],
+                                   [0., 0., 1., 0.],
+                                   [0., 0., 0., 1.]])
+        gaussian_3 = GaussianComponent(gaussian_mean_2, gaussian_cov_2, 0.1)
+
+        posterior_intensity = [gaussian_2, gaussian_3, gaussian_1]
+
+        posterior_intensity = merging(posterior_intensity, 0.5)
+
+        self.assertEqual(len(posterior_intensity), 2)
+        self.assertAlmostEqual(posterior_intensity[0].weight, 0.9)
+
+    def test_merge_intensity_with_five_gaussian_two_close_not_weight_sorted(self):
+        gaussian_1 = GaussianComponent(gaussian_mean, gaussian_cov, 0.6)
+        gaussian_2 = GaussianComponent(gaussian_mean, gaussian_cov, 0.3)
+        gaussian_mean_2 = np.array([0.5, 0.5, 0.2, 0.2]).transpose()
+        gaussian_cov_2 = np.array([[1., 0., 0., 0.],
+                                   [0., 1., 0., 0.],
+                                   [0., 0., 1., 0.],
+                                   [0., 0., 0., 1.]])
+        gaussian_3 = GaussianComponent(gaussian_mean_2, gaussian_cov_2, 0.1)
+        gaussian_4 = GaussianComponent(gaussian_mean_2, gaussian_cov_2, 0.1)
+
+        posterior_intensity = [gaussian_2, gaussian_3, gaussian_1, gaussian_4]
+
+        posterior_intensity = merging(posterior_intensity, 0.5)
+
+        self.assertEqual(len(posterior_intensity), 2)
+
+    def test_merge_empty_intensity(self):
+        posterior_intensity = []
+
+        posterior_intensity = merging(posterior_intensity, 0.5)
+
+        self.assertEqual(len(posterior_intensity), 0)
