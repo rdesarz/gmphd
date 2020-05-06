2715d5683badcc798b9f3472436268a252c2dd87
rdesarz <hib.pub@protonmail.com>
2020-05-06 18:34:02 +0200

    Add a new birth model

--- gmphd/birth_model.py
+++ gmphd/birth_model.py
@@ -1,20 +1,37 @@
 import numpy as np
 from numpy import float64
 from gmphd.gaussian_component import GaussianComponent
+import copy
 
 
-class BirthModel:
-    def __init__(self, width, depth):
+# Birth model which expects target to appear on the two lateral sides of a square field of view
+class SidesOfFovBirthModel:
+    def __init__(self, width, depth, step=1.0):
         self.width = width
         self.depth = depth
+        self.step = step
 
     def __call__(self):
         birth_intensity = list()
-        mean = np.array([5., 5., 0.5, 0.5], dtype=float64).transpose()
-        covariance = np.array([[1., 0., 0., 0.],
-                               [0., 1., 0., 0.],
-                               [0., 0., 1., 0.],
-                               [0., 0., 0., 1.]], dtype=float64)
-        weight = float64(0.1)
-        birth_intensity.append(GaussianComponent(mean=mean, covariance=covariance, weight=weight))
+
+        # Birth intensity on right side of FOV
+        for depth in np.arange(0., self.depth, self.step):
+            mean = np.array([self.width / 2., depth, -0.2, 0.1], dtype=float64).transpose()
+            covariance = np.array([[self.step, 0., 0., 0.],
+                                   [0., 0.2, 0., 0.],
+                                   [0., 0., 0.1, 0.],
+                                   [0., 0., 0., 0.1]], dtype=float64)
+            weight = float64(0.1)
+            birth_intensity.append(GaussianComponent(mean=mean, covariance=covariance, weight=weight))
+
+        # Birth intensity on left side of FOV
+        for depth in np.arange(0., self.depth, self.step):
+            mean = np.array([-self.width / 2., depth, 0.2, 0.1], dtype=float64).transpose()
+            covariance = np.array([[self.step, 0., 0., 0.],
+                                   [0., 0.2, 0., 0.],
+                                   [0., 0., 0.1, 0.],
+                                   [0., 0., 0., 0.1]], dtype=float64)
+            weight = float64(0.1)
+            birth_intensity.append(GaussianComponent(mean=mean, covariance=covariance, weight=weight))
+
         return birth_intensity
