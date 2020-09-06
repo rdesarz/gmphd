8fd9b75e5ac5c53cc3f3e8dbe4d0e5254d3f2d6b
Romain Desarzens <hib.pub@protonmail.com>
2020-09-06 12:29:49 +0200

    Update process noise model

--- python/gmphd/process_noises.py
+++ python/gmphd/process_noises.py
@@ -1,14 +1,13 @@
 import numpy as np
 
 
-class ConstantVelocityProcessNoise:
-    def __init__(self, process_noise_std_dev, get_delta_t):
-        self.get_delta_t = get_delta_t
+class ConstantVelocity:
+    def __init__(self, process_noise_std_dev):
         self.process_noise_std_dev = process_noise_std_dev
 
-    def __call__(self):
-        return np.array([[pow(self.get_delta_t, 4) / 4., 0., pow(self.get_delta_t, 3) / 2, 0.],
-                         [0., pow(self.get_delta_t, 4) / 4, 0., pow(self.get_delta_t, 3) / 2],
-                         [pow(self.get_delta_t, 3) / 2., 0., pow(self.get_delta_t, 2), 0.],
-                         [0., pow(self.get_delta_t, 3) / 2, 0., pow(self.get_delta_t, 2)]], dtype=np.float64).dot(
+    def __call__(self, delta_t):
+        return np.array([[pow(delta_t, 4) / 4., 0., pow(delta_t, 3) / 2, 0.],
+                         [0., pow(delta_t, 4) / 4, 0., pow(delta_t, 3) / 2],
+                         [pow(delta_t, 3) / 2., 0., pow(delta_t, 2), 0.],
+                         [0., pow(delta_t, 3) / 2, 0., pow(delta_t, 2)]], dtype=np.float64).dot(
             pow(self.process_noise_std_dev, 2))
