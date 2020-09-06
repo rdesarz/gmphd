8b9353f282bb241c9cdfcbe0555b803303219c10
Romain Desarzens <hib.pub@protonmail.com>
2020-09-06 12:29:33 +0200

    Update dynamic model

--- python/gmphd/dynamic_models.py
+++ python/gmphd/dynamic_models.py
@@ -2,11 +2,8 @@ import numpy as np
 
 
 class ConstantVelocity:
-    def __init__(self, get_delta_t):
-        self.get_delta_t = get_delta_t
-
-    def __call__(self):
-        return np.array([[1, 0., self.get_delta_t, 0.],
-                         [0., 1., 0., self.get_delta_t],
+    def __call__(self, delta_t):
+        return np.array([[1, 0., delta_t, 0.],
+                         [0., 1., 0., delta_t],
                          [0., 0., 1., 0.],
                          [0., 0., 0., 1.]], dtype=np.float64)
